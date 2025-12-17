9feb089e38b9c9fd6881def8dd30cee0a4fbc791
Andrew Vlietstra <vlietstra123@hotmail.com>
2025-12-17 16:16:21 -0500

    Setup project with Lightrays and home page

--- app/api/db.ts
+++
@@ -1,7 +0,0 @@
-const books = [
-	{ id: 1, name: "Atomic Habits" },
-	{ id: 2, name: "Deep Work" },
-	{ id: 3, name: "Eloquent JavaScript" },
-];
-
-export default books;
--- components/hello.tsx
+++
@@ -1,7 +0,0 @@
-"use client";
-
-const Hello = () => {
-	console.log("Hello from Client component");
-	return <div>Hello</div>;
-};
-export default Hello;
